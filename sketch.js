14b00dd8c0a593f3ba1fdfa1e6ce8b32186ff525
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2023-01-29 17:40:28 +0100

    Added variable grid and redraw button

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 function drawGrid(gridSize) {
-    const containerBox = document.querySelector('div#container');
+    const wrapperBox = document.querySelector('div#wrapper');
+    const containerBox = document.createElement('div');
+    containerBox.setAttribute('id', 'container');
+    wrapperBox.appendChild(containerBox);
+
     let id = 0;
 
     for (let i = 0; i < gridSize; i++) {
@@ -10,19 +14,43 @@ function drawGrid(gridSize) {
             const gridBox = document.createElement('div');
             gridBox.className = 'grid-item';
             gridBox.setAttribute('id', id++);
-            gridBox.style.width = 400/gridSize + 'px';
-            gridBox.style.height = 400/gridSize + 'px';
+            gridBox.style.width = (600 - (gridSize * 2))/gridSize + 'px';
+            gridBox.style.height = (600 - (gridSize * 2))/gridSize + 'px';
             gridBox.addEventListener('mouseover', colorGridBox);
             gridRow.appendChild(gridBox);
         }
-
         containerBox.appendChild(gridRow);
     }
 }
 
 function colorGridBox(e) {
-    console.log(e);
+    const targetBox = document.getElementById(e.target.id);
+    targetBox.classList.add('hovered');
+}
+
+function adjustGridSize() {
+
+    let newSize;
+
+    do {
+        newSize = prompt('Please enter a grid Size less than 100 boxes per side');
+
+    } while (newSize > 100 || newSize < 0 || isNaN(newSize))
+
+    const containerBox = document.getElementById('container');
+    containerBox.remove();
+
+    drawGrid(newSize);
+
 }
 
 
-drawGrid(16)
+function run() {
+    drawGrid(16);
+
+    document.querySelector('.btn').addEventListener('click', adjustGridSize);
+
+
+}
+
+run();
